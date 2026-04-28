cb04431f7bee29a6aa0e6edf61babe469fdeb138
Shariq Abbasi <shariqaliabbaxi@gmail.com>
2026-04-28 17:26:34 +0500

    Modify Chrome options for headless and debugging
    Updated Chrome options to use new headless mode and added remote debugging port.

--- firsttest.py
+++ firsttest.py
@@ -1,4 +1,3 @@
-# vim test.py
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -6,13 +5,12 @@ from selenium.webdriver.chrome.service import Service
 
 options = Options()
 options.add_argument('--no-sandbox')
-options.add_argument('--headless')
+options.add_argument('--headless=new')
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--remote-debugging-port=9222')
 
-service = Service('/usr/bin/chromedriver')  # Explicit path to chromedriver
+service = Service('/usr/bin/chromedriver')
 driver = webdriver.Chrome(service=service, options=options)
-
-#driver = webdriver.Chrome(options=options)
 
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
@@ -23,13 +21,3 @@ driver.implicitly_wait(5)
 
 text_box = driver.find_element(by=By.NAME, value="my-text")
 submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
-text_box.send_keys("Selenium")
-submit_button.click()
-
-message = driver.find_element(by=By.ID, value="message")
-value = message.text
-print(value)
-
-assert value == "Received!"
-driver.quit()
